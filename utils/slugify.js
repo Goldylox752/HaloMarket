7472e7810fe1da15d86bc47136499d8e3b9edd5e
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-03 09:54:57 -0600

    Create slugify.js

---
+++ utils/slugify.js
@@ -0,0 +1,19 @@
+// ==========================================
+// Slug Generator
+// ==========================================
+
+module.exports = function slugify(text = "") {
+
+    return text
+
+        .toLowerCase()
+
+        .trim()
+
+        .replace(/[^\w\s-]/g, "")
+
+        .replace(/\s+/g, "-")
+
+        .replace(/-+/g, "-");
+
+};
