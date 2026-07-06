a7e5d26baf4edfb1e032efbe5139fc7d73c0d7f4
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-06 12:18:36 -0600

    Update product.js

--- assets/js/pages/product.js
+++ assets/js/pages/product.js
@@ -1,20 +1,26 @@
-function loadProduct() {
+async function loadProduct() {
 
     const url = new URL(window.location.href);
     const id = url.searchParams.get("id");
 
-    const product = window.PRODUCTS.find(p => p.id === id);
+    try {
+        const res = await fetch(`/api/products/${id}`);
+        const product = await res.json();
 
-    if (!product) {
-        document.body.innerHTML = "<h2>Product not found</h2>";
-        return;
+        if (!res.ok) {
+            document.body.innerHTML = "<h2>Product not found</h2>";
+            return;
+        }
+
+        document.getElementById("title").innerText = product.title;
+        document.getElementById("image").src = product.image;
+        document.getElementById("price").innerText = "$" + product.price;
+
+        window.currentProduct = product;
+
+    } catch (err) {
+        console.error(err);
     }
-
-    document.getElementById("title").innerText = product.title;
-    document.getElementById("image").src = product.image;
-    document.getElementById("price").innerText = "$" + product.price;
-
-    window.currentProduct = product;
 }
 
 function addCurrentProduct() {
