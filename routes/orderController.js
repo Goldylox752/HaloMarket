c48a5d1f7e70200bc8c36f6263f562dd23111bfb
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-06 10:05:48 -0600

    Add orderController with public, vendor, and admin routes

---
+++ routes/orderController.js
@@ -0,0 +1,17 @@
+// Public
+createOrder
+getMyOrders
+getOrderById
+cancelOrder
+
+// Vendor
+getVendorOrders
+updateOrderStatus
+shipOrder
+completeOrder
+
+// Admin
+getAllOrders
+refundOrder
+deleteOrder
+getOrderAnalytics
