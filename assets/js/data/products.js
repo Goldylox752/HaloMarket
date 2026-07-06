0ca88540038e788d876ca4c134752eeb1ae13554
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-06 12:16:04 -0600

    Create products.js

---
+++ assets/js/data/products.js
@@ -0,0 +1,8 @@
+window.PRODUCTS = [
+    {
+        id: "p1",
+        title: "Apple iPhone 16 Pro",
+        price: 1199,
+        image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+    }
+];
