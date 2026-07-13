3dd70568ccaff0c7a222189d0015ab79545ce361
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-13 16:26:02 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,20 @@
-{
-  "compilerOptions": {
-    "target": "ES2017",
-    "lib": [
-      "dom",
-      "dom.iterable",
-      "esnext"
-    ],
-    "allowJs": true,
-    "skipLibCheck": true,
-    "strict": true,
-    "noEmit": true,
-    "esModuleInterop": true,
-    "module": "esnext",
-    "moduleResolution": "bundler",
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "jsx": "preserve",
-    "incremental": true,
+import type { NextConfig } from "next";
 
-    "plugins": [
+const nextConfig: NextConfig = {
+
+  images: {
+
+    remotePatterns: [
+
       {
-        "name": "next"
-      }
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+
     ],
 
-    "baseUrl": ".",
-    "paths": {
-      "@/*": [
-        "./*"
-      ]
-    }
   },
 
-  "include": [
-    "next-env.d.ts",
-    "**/*.ts",
-    "**/*.tsx",
-    ".next/types/**/*.ts"
-  ],
+};
 
-  "exclude": [
-    "node_modules"
-  ]
-}
+export default nextConfig;
